6af5c7568ecd6c991b70c7d777ec137d772d3c42
Omar <osaadoun@inmind.com.uy>
2019-10-08 11:54:01 -0300

    update

--- governance-aepp/src/views/Home.vue
+++ governance-aepp/src/views/Home.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="fixed w-full top-0 max-w-desktop">
       <BlackHeader :show-number-input="true" @submit="showPoll" @input="handleIdInput">
-        {{activeTab}} Encuestas
+        Encuestas
       </BlackHeader>
       <div class="flex bg-gray-ae text-gray-200">
         <div v-if="pollOrdering" :class="{active: activeTab === 'hot'}" @click="switchTab('hot')" class="tab">
@@ -202,7 +202,7 @@
 
 <style scoped>
   .bg-gray-ae {
-    background-color: #333333;
+    background-color: #FE8C00;
   }
 
   .tab {
