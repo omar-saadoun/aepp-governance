fd6b462235ff9e564a056f88727ee90276cb29f3
Omar <osaadoun@inmind.com.uy>
2019-10-12 08:05:46 -0300

    cambio server

--- governance-aepp/deploy.sh
+++ governance-aepp/deploy.sh
@@ -15,7 +15,7 @@ git checkout gh-pages && \
 git rm -rf . && \
 git clean -ffxd && \
 cp -r ../dist/* . && \
-echo "gobernanza.partidodigital.org.uy" > CNAME && \
+echo "gobernanza.inmind.space" > CNAME && \
 git add * && \
 git commit -m "governance aepp ${GIT_REV} deployment to gh-pages" && \
 git fetch && git rebase -s recursive -Xtheirs origin/gh-pages && \
